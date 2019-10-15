3c6c66c80a0bacb5f1cf74f341cf6bbe6f4811b0
fanzhoukai <fanzhoukai@163.com>
2019-10-15 20:07:20 +0800

    单例本地缓存工具类设计

--- src/main/java/com/learning/project/fzk/multiThread/cache/package-info.java
+++
@@ -1,7 +0,0 @@
-package com.learning.project.fzk.multiThread.cache;
-/*
- * 使用多种方式实现的缓存工具类包
- * Cache1：使用Future占位符实现，有多线程同时计算一个key的浪费问题
- * Cache2：使用ConcurrentHashMap#putIfAbsent实现
- * Cache3：使用ConcurrentHashMap#computeIfAbsent实现
- */
